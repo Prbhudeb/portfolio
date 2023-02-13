87fddc537ab4748f7de2691bc4e579a34525c85d
Prabhudev <prabhudev182002@gmail.com>
2023-02-13 16:59:41 +0530

    intial commit

---
+++ javascript/script.js
@@ -0,0 +1,6 @@
+
+let menuthing = document.querySelector(".menu-thing");
+function onClickMenu(){
+    document.getElementById("menu-to").classList.toggle("barr");
+    menuthing.classList.toggle("active");
+}
